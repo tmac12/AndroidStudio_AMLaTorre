2d4a34a1f35a71c4e5543d1181291e1c03fb6a0b
Marco <mba121@gmail.com>
2014-11-30 19:53:58 +0100

    Initial commit

---
+++ app/src/main/java/tmac12/it/amlatorre/MainActivity.java
@@ -0,0 +1,83 @@
+package tmac12.it.amlatorre;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Xml;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends Activity {
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
+
+    List<XmlParser.Partita> _partite = null;
+    private static LazyAdapter mLazyAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            CaricaDatiDaXML();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+        mLazyAdapter = new LazyAdapter(_partite, getLayoutInflater(),this);
+
+        mainListView.setAdapter(mLazyAdapter);
+    }
+
+    private void CaricaDatiDaXML() throws XmlPullParserException, IOException {
+        //List<XmlParser.Categoria> categorie = null;
+        XmlParser xmlParser = new XmlParser();
+        InputStream inputData = this.getResources().openRawResource(R.raw.data);
+        try {
+            _partite = xmlParser.parse(inputData);
+        }
+        finally {
+            if (inputData != null) {
+                inputData.close();
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/tmac12/it/amlatorre/XmlParser.java
@@ -0,0 +1,159 @@
+package tmac12.it.amlatorre;
+
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by marco on 30/11/14.
+ */
+public class XmlParser implements Serializable {
+    private static final String ns = null;
+
+
+    private List<Partita> readAMLaTorre(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List<Partita> entries = new ArrayList<Partita>();
+
+        parser.require(XmlPullParser.START_TAG, ns, "amlatorre");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            // Starts by looking for the entry tag
+            if (name.equals("evento")) {
+                entries.add(readPartita(parser));
+            } else {
+                skip(parser);
+            }
+        }
+        return entries;
+    }
+
+    public List<Partita> parse(InputStream in) throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+            parser.nextTag();
+            return readAMLaTorre(parser);
+        } finally {
+            in.close();
+        }
+    }
+
+    public static class Partita implements Serializable
+    {
+        public final String avversario;
+        public final String data;
+        public final String ora;
+        public final String luogo;
+
+        private Partita (String avversario, String data, String ora, String luogo){
+            this.avversario = avversario;
+            this.data = data;
+            this.ora = ora;
+            this.luogo = luogo;
+        }
+
+    }
+
+    // Parses the contents of an entry. If it encounters a title, summary, or link tag, hands them
+    // off
+    // to their respective &quot;read&quot; methods for processing. Otherwise, skips the tag.
+    private Partita readPartita(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "evento");
+        String data = null;
+        String ora = null;
+        String avversario = null;
+        String luogo=null;
+
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("data")) {
+                data = readData(parser);
+            } else if (name.equals("ora")) {
+                ora = readOra(parser);
+            } else if (name.equals("avversario")) {
+                avversario = readAvversario(parser);
+            } else if (name.equals("luogo")) {
+                luogo = readLuogo(parser);
+            } else {
+                skip(parser);
+            }
+        }
+        return new Partita(avversario, data, ora, luogo);
+    }
+
+    // Processes data tags in the feed.
+    private String readData(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "data");
+        String data = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "data");
+        return data;
+    }
+
+    // Processes data tags in the feed.
+    private String readOra(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "ora");
+        String ora = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "ora");
+        return ora;
+    }
+
+    // Processes data tags in the feed.
+    private String readAvversario(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "avversario");
+        String avversario = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "avversario");
+        return avversario;
+    }
+
+    // Processes data tags in the feed.
+    private String readLuogo(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "luogo");
+        String luogo = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "luogo");
+        return luogo;
+    }
+
+    // For the tags title and summary, extracts their text values.
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+
+    // Skips tags the parser isn't interested in. Uses depth to handle nested tags. i.e.,
+    // if the next tag after a START_TAG isn't a matching END_TAG, it keeps going until it
+    // finds the matching END_TAG (as indicated by the value of "depth" being 0).
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
+}
